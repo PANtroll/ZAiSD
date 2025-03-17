5b66ed1ae61489aa3c399f46ca683599f9ab3d9d
artur <zieloneimperium230@o2.pl>
2025-03-17 20:35:42 +0100

    progress

--- lista1/src/main/java/org/example/Macierz.java
+++ lista1/src/main/java/org/example/Macierz.java
@@ -3,7 +3,7 @@ package org.example;
 
 public class Macierz implements Fibonacci {
 
-    private long[][] fibMatrix, lastMatrix, tempMatrix;
+    private long[][] fibMatrix, resultMatrix;
 
     @Override
     public long calculateFibonacci(int n) {
@@ -15,27 +15,29 @@ public class Macierz implements Fibonacci {
         fibMatrix[0][1] = 1;
         fibMatrix[1][0] = 1;
         fibMatrix[1][1] = 0;
-        lastMatrix = new long[2][2];
-        tempMatrix = new long[2][2];
-        clone(fibMatrix, lastMatrix);
-        for (int i = 2; i < n; i++) {
-            tempMatrix[0][0] = multiply(lastMatrix[0][0], fibMatrix[0][0]) + multiply(lastMatrix[0][1], fibMatrix[1][0]);
-            tempMatrix[0][1] = multiply(lastMatrix[0][0], fibMatrix[0][1]) + multiply(lastMatrix[0][1], fibMatrix[1][1]);
-            tempMatrix[1][0] = multiply(lastMatrix[1][0], fibMatrix[0][0]) + multiply(lastMatrix[1][1], fibMatrix[1][0]);
-            tempMatrix[1][1] = multiply(lastMatrix[1][0], fibMatrix[0][1]) + multiply(lastMatrix[1][1], fibMatrix[1][1]);
-            clone(tempMatrix, lastMatrix);
-        }
-        return lastMatrix[0][0];
+        resultMatrix = new long[2][2];
+        resultMatrix = quickPower(fibMatrix, n-1);
+        return resultMatrix[0][0];
     }
 
-    private long multiply(long x, long y) {
+    private long[][] quickPower(long[][] matrix, int n) {
+        if (n == 1) return matrix;
+        long[][] y = quickPower(matrix, n / 2);
+        y = multiplyMatrix(y, y);
+        if (n % 2 == 1) y = multiplyMatrix(y, matrix);
+        return y;
+    }
+
+    private long[][] multiplyMatrix(long[][] lastMatrix, long[][] fibMatrix) {
+        long[][] tempMatrix = new long[2][2];
+        tempMatrix[0][0] = multiplyScalars(lastMatrix[0][0], fibMatrix[0][0]) + multiplyScalars(lastMatrix[0][1], fibMatrix[1][0]);
+        tempMatrix[0][1] = multiplyScalars(lastMatrix[0][0], fibMatrix[0][1]) + multiplyScalars(lastMatrix[0][1], fibMatrix[1][1]);
+        tempMatrix[1][0] = multiplyScalars(lastMatrix[1][0], fibMatrix[0][0]) + multiplyScalars(lastMatrix[1][1], fibMatrix[1][0]);
+        tempMatrix[1][1] = multiplyScalars(lastMatrix[1][0], fibMatrix[0][1]) + multiplyScalars(lastMatrix[1][1], fibMatrix[1][1]);
+        return tempMatrix;
+    }
+
+    private long multiplyScalars(long x, long y) {
         return Math.floorMod(x * y, MOD);
-    }
-
-    private void clone(long[][] source, long[][] target) {
-        target[0][0] = source[0][0];
-        target[0][1] = source[0][1];
-        target[1][0] = source[1][0];
-        target[1][1] = source[1][1];
     }
 }
