16ac1ea2c1ffbf23adea24233ef168aa08139825
artur <zieloneimperium230@o2.pl>
2025-03-18 20:37:59 +0100

    commit

--- lista1/src/main/java/pl/zaisd/Main.java
+++ lista1/src/main/java/pl/zaisd/Main.java
@@ -6,26 +6,27 @@ public class Main {
         Fibonacci tablica = new Tablica();
         Fibonacci rekurencja = new Rekurencja();
         Fibonacci macierz = new Macierz();
-        int number = 60;
-//        int number = 10_000;
-        for (int i = 10; i <= number; i+=10) {
-        runWithStopwatch(rekurencja, i);
-        runWithStopwatch(tablica, i);
-        runWithStopwatch(macierz, i);
-    }
+//        int number = 50;
+        int number = 10_000_000;
+        int increment = 100;
+        for (int i = 1_000; i <= number; i += increment) {
+//            runWithStopwatch(rekurencja, i);
+            runWithStopwatch(tablica, i);
+            runWithStopwatch(macierz, i);
+            if(i%(increment*10)==0)
+                increment = increment * 10;
+        }
     }
 
-    private static void runWithStopwatch(Fibonacci rekurencja, int number) {
+    private static void runWithStopwatch(Fibonacci method, int number) {
 //        long startTime = System.currentTimeMillis();
         long startTime = System.nanoTime();
-        long calculatedFibonacci1 = rekurencja.calculateFibonacci(number);
-        long calculatedFibonacci2 = rekurencja.calculateFibonacci(number);
-        long calculatedFibonacci3 = rekurencja.calculateFibonacci(number);
-        if(calculatedFibonacci1 != calculatedFibonacci2 || calculatedFibonacci2 != calculatedFibonacci3) {
-            throw new RuntimeException("Fibonacci numbers do not match");
+        long calculatedFibonacci = 0;
+        for (int i = 0; i < 10; i++) {
+            calculatedFibonacci = method.calculateFibonacci(number);
         }
 //        long duration = System.currentTimeMillis() - startTime;
-        long duration = (System.nanoTime() - startTime)/3;
-        System.out.println("Fibonacci for: " + number + " is: " + calculatedFibonacci3 + " in " + duration + "ms");
+        long duration = (System.nanoTime() - startTime) / 10_000;// 10 invokes to micro
+        System.out.println("Fibonacci for: " + number + " is: " + calculatedFibonacci + " in " + duration + "μs");
     }
 }
