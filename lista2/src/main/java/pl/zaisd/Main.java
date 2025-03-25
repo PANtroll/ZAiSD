f614251cd716ae1dd3b5b46195ed6758e6ea5bf3
artur <zieloneimperium230@o2.pl>
2025-03-25 19:16:15 +0100

    progress

--- lista2/src/main/java/pl/zaisd/Main.java
+++ lista2/src/main/java/pl/zaisd/Main.java
@@ -1,21 +1,40 @@
 package pl.zaisd;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static final String DELIMITER = ",";
+
+    public static void main(String[] args) throws IOException {
         PrimeNumbers iteration = new Iteration();
         PrimeNumbers millerRabin = new MillerRabinAlgorithm();
-        long max = 20;
-//        long max = 100_000;
-        for (long i = 4; i < max; i++) {
-            runWithStopwatch(iteration, i);
-            runWithStopwatch(millerRabin, i);
+
+        File path = new File("dane.csv");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+
+//        long max = 20;
+        long min = 511L;
+        long max = (long) Math.pow(10, 10) /3;//18
+        Random random = new Random();
+//        System.out.println(MillerRabinAlgorithm.quickPower(2, 2, 10));
+//        System.out.println(MillerRabinAlgorithm.quickPower(3, 3, 10));
+        for (; min <= max; min += random.nextInt(1_000, 1_000_000) * 2L) {
+            Tuple result1 = runWithStopwatch(iteration, min);
+            Tuple result2 = runWithStopwatch(millerRabin, min);
+            if (result1.isPrime || result2.isPrime)
+                writer.write(min + DELIMITER + result1.isPrime + DELIMITER + result1.duration +
+                        DELIMITER + result2.isPrime + DELIMITER + result2.duration + "\r\n");
             System.out.println();
         }
-
+        writer.close();
     }
 
-    private static void runWithStopwatch(PrimeNumbers method, long number) {
+    private static Tuple runWithStopwatch(PrimeNumbers method, long number) throws IOException {
         long startTime = System.nanoTime();
         boolean result = false;
         for (int i = 0; i < 10; i++) {
@@ -23,5 +42,24 @@ public class Main {
         }
         long duration = (System.nanoTime() - startTime) / 10_000;// 10 invokes converted to micro
         System.out.println("Number: " + number + " isPrime: " + result + " in " + duration + "μs");
+        return new Tuple(duration, result);
+    }
+
+    private static class Tuple {
+        private final long duration;
+        private final boolean isPrime;
+
+        public Tuple(long duration, boolean isPrime) {
+            this.duration = duration;
+            this.isPrime = isPrime;
+        }
+
+        public long getDuration() {
+            return duration;
+        }
+
+        public boolean isPrime() {
+            return isPrime;
+        }
     }
 }
--- lista2/src/main/java/pl/zaisd/MillerRabinAlgorithm.java
+++ lista2/src/main/java/pl/zaisd/MillerRabinAlgorithm.java
@@ -4,30 +4,48 @@ import java.util.Random;
 
 public class MillerRabinAlgorithm implements PrimeNumbers {
 
+    public static final long MAX_ITERATION = 100;
+
     @Override
     public boolean isPrime(long n) {
         if (n < 2) return false;
         if (n == 2) return true;
         if (n % 2 == 0) return false;
+        if (n == 3) return true;
 
         Random r = new Random();
         long powerIndex = 0;//s
         long powerMultiplier = n - 1;//d
-        while (powerMultiplier % 2 == 0) {
-            powerIndex++;
+        for (powerMultiplier = n-1; powerMultiplier % 2 == 0; powerIndex++) {
             powerMultiplier = powerMultiplier / 2;
         }
-        long maxIteration = 100;
-        for (long i = 0; i < maxIteration; i++) {
+        for (long i = 0; i < MAX_ITERATION; i++) {
             long random = r.nextLong(2, n - 2);//a
-            if (Math.pow(random, powerMultiplier) % n != 1) {
-                for (int j = 0; j < powerIndex; j++) {
-                    if (Math.pow(random, powerMultiplier * Math.pow(2, j)) % n == 1) {
-                        return false;
+            long x = quickPower(random, powerMultiplier, n);
+            if (x != 1 && x != n-1) {
+                for (int j = 0; j < powerIndex - 1; j++) {
+                    x = (x * x) % n;
+                    if (x == n - 1) {
+                        break;
                     }
+                }
+                if (x != n - 1) {
+                    return false;
                 }
             }
         }
         return true;
+    }
+    public static long quickPower(long x, long y, long mod) {
+        long result = 1;
+        x = x % mod;
+        while (y > 0) {
+            if (y % 2 == 1) {
+                result = (result * x) % mod;
+            }
+            y = y / 2;
+            x = (x * x) % mod;
+        }
+        return result;
     }
 }
