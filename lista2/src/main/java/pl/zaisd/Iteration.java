472f607e61b9dcb395d8c7954f3be0b22bbba62a
artur <zieloneimperium230@o2.pl>
2025-04-06 12:39:43 +0200

    add documentation

--- lista2/src/main/java/pl/zaisd/Iteration.java
+++ lista2/src/main/java/pl/zaisd/Iteration.java
@@ -3,7 +3,7 @@ package pl.zaisd;
 public class Iteration implements PrimeNumbers {
     @Override
     public boolean isPrime(long n) {
-        long max = (long) Math.sqrt(n);
+        long max = (long) Math.sqrt(n) + 1;
         for (long i = 2L; i <= max; i++) {
             if(n % i == 0L) {
                 return false;
