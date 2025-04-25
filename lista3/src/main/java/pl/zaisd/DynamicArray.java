b5abf652d7544fd9cfd5c446f7f2e100a3c5d165
artur <zieloneimperium230@o2.pl>
2025-04-25 18:01:39 +0200

    lab 3 - fix pushes

---
+++ lista3/src/main/java/pl/zaisd/DynamicArray.java
@@ -0,0 +1,90 @@
+package pl.zaisd;
+
+import java.util.Arrays;
+
+public class DynamicArray<T> {
+
+    public static final int MIN_SIZE = 2;
+    T[] array;
+
+    int size;
+
+    public DynamicArray() {
+        this.array = (T[]) new Object[MIN_SIZE];
+        this.size = 0;
+    }
+
+    public DynamicArray(int size) {
+        this.array = (T[]) new Object[size];
+        this.size = 0;
+    }
+
+    public T at(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return array[index];
+    }
+
+    public void append(T newElement) {
+        if (size == array.length) {
+            extendArray();
+        }
+        array[size] = newElement;
+        size++;
+    }
+
+    public T cut() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        T result = array[size];
+        array[size] = null;
+        if (size <= array.length / 4) {
+            downgradeArray();
+        }
+        return result;
+    }
+    public int size() {
+        return size;
+    }
+
+    private void extendArray() {
+        T[] newArray = (T[]) new Object[size * 2];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
+
+    private void downgradeArray() {
+        if(array.length <= MIN_SIZE) {
+            return;
+        }
+        T[] newArray = (T[]) new Object[array.length / 2];
+        System.arraycopy(array, 0, newArray, 0, array.length / 4);
+        array = newArray;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("DynamicArray{" + "array=[");
+        result.append("]").append(", capacity=").append(size);
+        result.append(", length=").append(array.length).append('}');
+        return result.toString();
+    }
+
+    public String toFullArrayString() {
+        return "DynamicArray{array=" + Arrays.toString(array) + ", capacity=" + size + ", length=" + array.length + '}';
+    }
+
+    public String toCSV(){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            result.append(array[i]);
+            if (i != size - 1) {
+                result.append(",");
+            }
+        }
+        return result.toString();
+    }
+}
---
+++ lista3/src/main/java/pl/zaisd/Main.java
@@ -0,0 +1,126 @@
+package pl.zaisd;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+
+    public static final String DELIMITER = ";";
+
+    public static void main(String[] args) throws IOException {
+        List<Integer> arrayList = new ArrayList<>();
+        DynamicArray<Integer> dynamicArray = new DynamicArray<>();
+
+        File path = new File("dane.csv");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(path));
+
+        int max = 100_000;
+        for (int i = 100; i < max; i += 300) {
+            runWithStopwatch(i, dynamicArray, writer);
+            runWithStopwatch(i, arrayList, writer);
+            System.out.println("progress: " + i * 100 / max);
+        }
+        writer.close();
+    }
+
+    private static void runWithStopwatch(int max, DynamicArray<Integer> dynamicArray, BufferedWriter writer) throws IOException {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 5; i++) {
+            doSomeOperationOnArray(max, dynamicArray);
+        }
+        double duration = (System.nanoTime() - startTime) / 5_000_000.0;// 5 invokes
+        String durationStr = (DELIMITER + duration).replace('.', ',');
+        writer.write("DynamicArray" + DELIMITER + max + durationStr + DELIMITER + "\r\n");
+        System.out.println("Duration: " + duration + "ms");
+    }
+
+    private static void runWithStopwatch(int max, List<Integer> arrayList, BufferedWriter writer) throws IOException {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < 5; i++) {
+            doSomeOperationOnList(max, arrayList);
+        }
+        double duration = (System.nanoTime() - startTime) / 5_000_000.0;// 5 invokes
+        String durationStr = (DELIMITER + duration).replace('.', ',');
+        writer.write("ArrayList" + DELIMITER + max + durationStr + DELIMITER + "\r\n");
+        System.out.println("Duration: " + duration + "ms");
+    }
+
+    private static void doSomeOperationOnArray(int max, DynamicArray<Integer> dynamicArray) throws IOException {
+        for (int i = 0; i < max; i++) {
+            dynamicArray.append(i);
+            dynamicArray.append(i);
+            dynamicArray.cut();
+//            System.out.println(dynamicArray.toFullArrayString());
+//            System.out.println(dynamicArray);
+//            System.out.println();
+        }
+        for (int i = 0; i < max; i++) {
+            dynamicArray.cut();
+            dynamicArray.append(i);
+            dynamicArray.append(i);
+            dynamicArray.cut();
+            dynamicArray.append(i);
+            dynamicArray.cut();
+            dynamicArray.append(i);
+            dynamicArray.append(i);
+            dynamicArray.cut();
+            dynamicArray.cut();
+//            System.out.println(dynamicArray.toFullArrayString());
+//            System.out.println(dynamicArray);
+//            System.out.println();
+        }
+        Random r = new Random();
+        for (int i = 0; i < 4; i++) {
+            dynamicArray.at(r.nextInt(dynamicArray.size()));
+        }
+        for (int i = max; i > 0; i--) {
+            dynamicArray.append(i);
+            dynamicArray.cut();
+            dynamicArray.cut();
+//            System.out.println(dynamicArray.toFullArrayString());
+//            System.out.println(dynamicArray);
+//            System.out.println();
+        }
+    }
+
+    private static void doSomeOperationOnList(int max, List<Integer> arrayList) throws IOException {
+        for (int i = 0; i < max; i++) {
+            arrayList.add(i);
+            arrayList.add(i);
+            arrayList.remove(arrayList.size() - 1);
+//            System.out.println(arrayList);
+//            System.out.println();
+        }
+        for (int i = 0; i < max; i++) {
+            arrayList.remove(arrayList.size() - 1);
+            arrayList.add(i);
+            arrayList.add(i);
+            arrayList.remove(arrayList.size() - 1);
+            arrayList.add(i);
+            arrayList.remove(arrayList.size() - 1);
+            arrayList.add(i);
+            arrayList.add(i);
+            arrayList.remove(arrayList.size() - 1);
+            arrayList.remove(arrayList.size() - 1);
+//            System.out.println(arrayList);
+//            System.out.println();
+        }
+        Random r = new Random();
+        for (int i = 0; i < 4; i++) {
+            arrayList.get(r.nextInt(arrayList.size()));
+        }
+        for (int i = max; i > 0; i--) {
+            arrayList.add(i);
+            arrayList.remove(arrayList.size() - 1);
+            arrayList.remove(arrayList.size() - 1);
+//            System.out.println(arrayList);
+//            System.out.println();
+        }
+    }
+
+}
