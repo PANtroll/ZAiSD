b37426ed40f0cc41a7d9b9877aefd015b2b22ee7
artur <zieloneimperium230@o2.pl>
2025-06-20 16:45:08 +0200

    list 6 - progress

--- lista6/src/main/java/pl/zaisd/HashArrayDouble.java
+++ lista6/src/main/java/pl/zaisd/HashArrayDouble.java
@@ -1,12 +1,12 @@
 package pl.zaisd;
 
+import java.util.Objects;
+
 public class HashArrayDouble<V> extends AbstractHashArray<V> {
     @Override
     protected int hashConflictIncrease(int hash, int attempt) {
-        if(hash == 0){
-            return 1;
-        }
-        int newHash = ((hash + (2 * attempt)) % capacity());
+        int hash2 = 1 + (Objects.hashCode(hash) % (capacity() - 1));
+        int newHash = ((hash + (attempt * hash2)) % capacity());
         return newHash;
     }
     @Override
