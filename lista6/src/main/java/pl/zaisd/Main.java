a7f5f2dd6629d4d7963c623dfbe8ca04c8af6737
artur <zieloneimperium230@o2.pl>
2025-06-20 16:56:27 +0200

    list 6 - progress

--- lista6/src/main/java/pl/zaisd/Main.java
+++ lista6/src/main/java/pl/zaisd/Main.java
@@ -17,13 +17,15 @@ public class Main {
         HashArray<Integer> hashArrayLine = new HashArrayLine<>();
         HashArray<Integer> hashArrayDouble = new HashArrayDouble<>();
         HashArray<Integer> hashArraySquare = new HashArraySquare<>();
+        Set<Integer> set = new HashSet<>();
 
         File path = new File("lista6/dane.csv");
         BufferedWriter writer = new BufferedWriter(new FileWriter(path));
-        for (int i = 10; i < 100_000; i = i*2) {
+        for (int i = 10; i < 100_000; i = i * 2) {
             runWithStopwatch(hashArrayLine, writer, i);
             runWithStopwatch(hashArrayDouble, writer, i);
             runWithStopwatch(hashArraySquare, writer, i);
+            runWithStopwatch(set, writer, i);
         }
         writer.close();
     }
@@ -38,16 +40,16 @@ public class Main {
             values.add(value);
             hashArray.insert(value);
         }
-        for (Integer value: values){
+        for (Integer value : values) {
             Object searchedObject = hashArray.search(value);
-            if(searchedObject == null || !value.equals(searchedObject)){
+            if (searchedObject == null || !value.equals(searchedObject)) {
                 throw new IllegalStateException("Object not found");
             }
         }
         for (int i = 0; i < iterations; i++) {
             Integer value = r.nextInt();
             Object searchedObject = hashArray.search(value);
-            if(values.contains(value) && !value.equals(searchedObject)){
+            if (values.contains(value) && !value.equals(searchedObject)) {
                 throw new IllegalStateException("Object not found");
             }
         }
@@ -58,4 +60,34 @@ public class Main {
         System.out.println(hashArray + ", duration: " + duration + "ms");
 //        System.out.println("Result: " + result);
     }
+    private static void runWithStopwatch(Set set, BufferedWriter writer, int iterations) throws IOException {
+        int result = 0;
+        Random r = new Random();
+        List<Integer> values = new LinkedList<>();
+        long startTime = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            int value = r.nextInt();
+            values.add(value);
+            set.add(value);
+        }
+        for (Integer value : values) {
+            boolean searchedObject = set.contains(value);
+            if (!searchedObject) {
+                throw new IllegalStateException("Object not found");
+            }
+        }
+        for (int i = 0; i < iterations; i++) {
+            Integer value = r.nextInt();
+            boolean searchedObject = set.contains(value);
+            if (values.contains(value) && !searchedObject) {
+                throw new IllegalStateException("Object not found");
+            }
+        }
+
+        double duration = (System.nanoTime() - startTime) / 5_000_000.0;// 5 invokes
+        String durationStr = (DELIMITER + duration).replace('.', ',');
+        writer.write("HashSet" + durationStr + DELIMITER + iterations + "\r\n");
+        System.out.println("HashSet" + ", duration: " + duration + "ms");
+//        System.out.println("Result: " + result);
     }
+}
